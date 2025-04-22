8be4a04e2f4aae0dbb4190443bcce966542b949e
Pablo Villarreal <pablovillarreal@Mac.attlocal.net>
2025-04-22 15:30:40 -0700

    fixed the button issue

--- app/components/ui/button/button.tsx
+++ app/components/ui/button/button.tsx
@@ -1,7 +1,11 @@
-export default function Button() {
+
+
+function Button() {
     return (
-      <button className="bg-blue-600 text-white px-4 py-2 rounded">
-        Click Me
-      </button>
+        <button className="bg-blue-600 text-white px-4 py-2 rounded w-[400px] h-[52px] border-2 border-white">
+            Click Me
+        </button>
     );
-  }
+}
+
+export default Button;
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import Button from "./components/ui/button/button";
 
 function Home() {
     return (
-        <div>
-            <h1 className="text-4xl font-bold flex justify-center items-center h-screen">Hello world, we innnn this bitch.</h1>
-            <Button />
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="m-16 text-4xl font-bold">Hello world, we innnn this bitch.</h1>
         </div>
     )
 }
