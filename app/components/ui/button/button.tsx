95f88d8ef1ab964ec4b51af51c2cba522bfe8c5c
Pablo Villareal <pablovillareal@Pablos-Opus-Mac.local>
2025-04-21 18:58:28 -0700

    next push

--- app/components/ui/button/button.tsx
+++ app/components/ui/button/button.tsx
@@ -1,42 +1,7 @@
-'use client';
-
-import { MouseEvent, ReactNode } from 'react';
-
-interface ButtonProps {
-  children: ReactNode;
-  variant?: 'primary' | 'secondary' | 'outline';
-  disabled?: boolean;
-  fullWidth?: boolean;
-  onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
-}
-
-export default function Button({
-    children,
-    variant = 'primary',
-    disabled = false,
-    fullWidth = false,
-    onClick,
-  }: ButtonProps) {
-    let style = '';
-  
-    if (variant === 'primary') {
-      style = 'bg-blue-600 text-white hover:bg-blue-700';
-    } else if (variant === 'secondary') {
-      style = 'bg-gray-200 text-black hover:bg-gray-300';
-    } else if (variant === 'outline') {
-      style = 'border border-gray-500 text-black hover:bg-gray-100';
-    }
-  
-    const width = fullWidth ? 'w-full' : '';
-    const disabledStyle = disabled ? 'opacity-50 cursor-not-allowed' : '';
-  
+export default function Button() {
     return (
-      <button
-        disabled={disabled}
-        onClick={onClick}
-        className={`px-4 py-2 rounded ${style} ${width} ${disabledStyle}`}
-      >
-        {children}
+      <button className="bg-blue-600 text-white px-4 py-2 rounded">
+        Click Me
       </button>
     );
   }
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Button from "./components/ui/button/button";
 function Home() {
     return (
         <div>
-            <h1 className="text-4xl font-bold flex justify-center items-center h-screen">Hello world, we in this bitch.</h1>
-            <Button>Click Me</Button>
+            <h1 className="text-4xl font-bold flex justify-center items-center h-screen">Hello world, we innnn this bitch.</h1>
+            <Button />
         </div>
     )
 }
