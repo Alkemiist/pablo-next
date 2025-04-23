053d804e8b680be3184e1f466d8366cebd7d42af
Pablo Villareal <pablovillareal@Mac.attlocal.net>
2025-04-23 14:30:19 -0700

    added state change to the button -- leveling up bro

--- app/components/ui/button/button.tsx
+++ app/components/ui/button/button.tsx
@@ -1,11 +1,14 @@
+'use client';
 
+import { useState } from 'react';
 
 // these are the props that the button component will receive
 type ButtonProps = {
   children: React.ReactNode; // this is the button text
-  onClick?: () => void; // this is the function that will be called when the button is clicked
+  onClick?: () => void; // Defining the onClick function
   variant?: "primary" | "secondary"; // this is the variant of the button
   disabled?: boolean; // this is the disabled state of the button
+  size?: 'small' | 'medium' | 'large'; // this is the size of the button
 }
 
 // this is the button component
@@ -14,15 +17,29 @@ export default function Button({
   onClick, 
   variant = "primary", 
   disabled = false,
+  size = 'large',
  }: ButtonProps) {
+
+  // this is the state of the button
 
   let styles = '';
 
   // these are the styles for the button depending on the variant
   if ( variant === "primary" ) {
-    styles = 'bg-blue-600 text-white px-4 py-2 rounded w-[400px] h-[52px]';
+    styles = 'bg-blue-600 text-white px-4 py-2 rounded';
   } else if ( variant === "secondary" ) {
-    styles = 'bg-gray-600 text-white px-4 py-2 rounded w-[400px] h-[52px]';
+    styles = 'bg-gray-600 text-white px-4 py-2 rounded ';
+  }
+
+  //these are the sizes for the button
+let buttonSize = '';
+
+  if( size === 'small' ) {
+    buttonSize = 'px-4 py-2 rounded w-[100px] h-[32px]';
+  } else if( size === 'medium' ) {
+    buttonSize = 'px-4 py-2 rounded w-[200px] h-[42px]';
+  } else if( size === 'large' ) {
+    buttonSize = 'px-4 py-2 rounded w-[400px] h-[52px]';
   }
 
   // this is the disabled state of the button
@@ -31,12 +48,11 @@ export default function Button({
   return (
 
     <button 
-    className={`${styles} ${disabledStyles} rounded-2xl`} 
-    onClick={disabled ? undefined : onClick}
+      className={`${styles} ${disabledStyles} ${buttonSize} rounded font-bold`} 
+      onClick={disabled ? undefined : onClick}
     >
       {children}
     </button>
    
   )
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+'use client';
+
+// imports
+import { useState } from 'react';
 import Button from "./components/ui/button/button";
 
+
+// this is the home component
 function Home() {
+
+    // this is the state of the count
+    const [ count, setCount ] = useState(0);
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="m-16 text-4xl font-bold">Hello world, we innnn this bitch</h1>
-            <Button variant="primary" disabled={false} >Learn More</Button>
+            <h1 className="m-16 text-4xl font-bold">Click to Count</h1>
+            <Button variant="primary" disabled={false} size="large" onClick={() => setCount(count + 1)}>You clicked {count} times</Button>
         </div>
     )
 }
