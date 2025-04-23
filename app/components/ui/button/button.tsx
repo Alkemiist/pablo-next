65381b980b6d4b11f59fe6d9a638237866852fe2
Pablo Villarreal <pablovillarreal@Mac.attlocal.net>
2025-04-22 22:47:15 -0700

    really understand the button situation

--- app/components/ui/button/button.tsx
+++ app/components/ui/button/button.tsx
@@ -1,10 +1,42 @@
 
-export default function Button({ children }: { children: React.ReactNode }) {
+
+// these are the props that the button component will receive
+type ButtonProps = {
+  children: React.ReactNode; // this is the button text
+  onClick?: () => void; // this is the function that will be called when the button is clicked
+  variant?: "primary" | "secondary"; // this is the variant of the button
+  disabled?: boolean; // this is the disabled state of the button
+}
+
+// this is the button component
+export default function Button({ 
+  children, 
+  onClick, 
+  variant = "primary", 
+  disabled = false,
+ }: ButtonProps) {
+
+  let styles = '';
+
+  // these are the styles for the button depending on the variant
+  if ( variant === "primary" ) {
+    styles = 'bg-blue-600 text-white px-4 py-2 rounded w-[400px] h-[52px]';
+  } else if ( variant === "secondary" ) {
+    styles = 'bg-gray-600 text-white px-4 py-2 rounded w-[400px] h-[52px]';
+  }
+
+  // this is the disabled state of the button
+  const disabledStyles = disabled ? 'opacity-50 cursor-not-allowed' : '';
+
   return (
-    <button className="bg-blue-600 text-white px-4 py-2 rounded w-[400px] h-[52px]">
+
+    <button 
+    className={`${styles} ${disabledStyles} rounded-2xl`} 
+    onClick={disabled ? undefined : onClick}
+    >
       {children}
     </button>
+   
   )
 }
 
-// children is a prop that is passed to the button component -- this makes the button component reusable
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Button from "./components/ui/button/button";
 
 function Home() {
+
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="m-16 text-4xl font-bold">Hello world, we innnn this bitch</h1>
-            <Button>Click Me MF</Button>
+            <Button variant="primary" disabled={false} >Learn More</Button>
         </div>
     )
 }
