ba4813891e56c4ebd907833c3b064da20dc66038
Pablo Villarreal <pablovillarreal@Mac.attlocal.net>
2025-04-22 21:59:21 -0700

    fixed the button issues

--- app/components/ui/button/button.tsx
+++ app/components/ui/button/button.tsx
@@ -1,11 +1,10 @@
 
-
-function Button() {
-    return (
-        <button className="bg-blue-600 text-white px-4 py-2 rounded w-[400px] h-[52px] border-2 border-white">
-            Click Me
-        </button>
-    );
+export default function Button({ children }: { children: React.ReactNode }) {
+  return (
+    <button className="bg-blue-600 text-white px-4 py-2 rounded w-[400px] h-[52px]">
+      {children}
+    </button>
+  )
 }
 
-export default Button;
+// children is a prop that is passed to the button component -- this makes the button component reusable
