59f688f1dc0ae87d3c72c01b198812a5a5615385
Pablo Villarreal <pablovillarreal@Mac.attlocal.net>
2025-05-08 13:10:03 -0700

    made changes

--- app/components/global/intelligence-top-bar.tsx
+++ app/components/global/intelligence-top-bar.tsx
@@ -32,7 +32,7 @@ export default function IntelligenceTopBar() {
             <div className="flex-1 flex gap-8 text-left relative items-center">
 
                 {/* the search input */}
-                <input type="search" placeholder="Search" className="hidden lg:flex bg-slate-900 w-full lg:w-1/2 border pl-12 border-slate-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:ring-white transition-colors duration-300" />
+                <input type="search" placeholder="Search" className="hidden lg:flex bg-slate-900 lg:w-1/4 border pl-12 border-slate-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:ring-white transition-colors duration-300" />
                 <Search className="hidden lg:flex w-4 h-4 absolute left-4 top-1/2 -translate-y-1/2 text-slate-400" />
 
                 {/* the filter button */}
@@ -40,25 +40,33 @@ export default function IntelligenceTopBar() {
                     <button className='hidden md:flex items-center gap-2 cursor-pointer' onClick={ () => setFilterOpen( !filterOpen ) }><ListFilter className="w-4 h-4" />Filter<ChevronDown className={`w-4 h-4 transition-transform duration-300 ${ filterOpen ? 'rotate-180' : 'opacity-50' }`} /></button>
                     {
                         filterOpen && (
-                            <div className="absolute flex flex-col border mt-8 md:w-60 bg-slate-900 border-slate-700 text-white rounded-md">
-                                <a href="#" className="hover:bg-slate-800 transition-colors duration-300 rounded-md px-4 py-4">Filter Number One</a> 
-                                <a href="#" className="hover:bg-slate-800 transition-colors duration-300 rounded-md px-4 py-4">Filter Number Two</a>
-                                <a href="#" className="hover:bg-slate-800 transition-colors duration-300 rounded-md px-4 py-4">Filter Number Three</a>
-                                <a href="#" className="hover:bg-slate-800 transition-colors duration-300 rounded-md px-4 py-4">Filter Number Four</a>
+                            <div className="absolute flex flex-col border mt-4 md:w-60 bg-slate-900 border-slate-700 text-white rounded-md">
+                                <a href="#" className="hover:bg-slate-800 transition-colors duration-300 px-4 py-4">Filter Number One</a> 
+                                <a href="#" className="hover:bg-slate-800 transition-colors duration-300 px-4 py-4">Filter Number Two</a>
+                                <a href="#" className="hover:bg-slate-800 transition-colors duration-300 px-4 py-4">Filter Number Three</a>
+                                <a href="#" className="hover:bg-slate-800 transition-colors duration-300 px-4 py-4">Filter Number Four</a>
+                            </div>
+                        )
+                    } 
+                </div>
+
+                {/* the sort button */}
+                <div className="relative">
+                    <button className='hidden md:flex items-center gap-2 cursor-pointer' onClick={ () => setSortOpen( !sortOpen ) }><ArrowDownUp className="w-4 h-4 " />Sort<ChevronDown className={`w-4 h-4 transition-transform duration-300 ${ sortOpen ? 'rotate-180' : 'opacity-50' }`} /></button>
+                    {
+                        sortOpen && (
+                            <div className="absolute flex flex-col border mt-4 md:w-60 bg-slate-900 border-slate-700 text-white rounded-md">
+                                <a href="#" className="hover:bg-slate-800 transition-colors duration-300 px-4 py-4">Sort Number One</a>
+                                <a href="#" className="hover:bg-slate-800 transition-colors duration-300 px-4 py-4">Sort Number Two</a>
                             </div>
                         )
                     }
                 </div>
 
-                {/* the sort button */}
-                <div className="relative">
-                    <button className='hidden md:flex items-center gap-2 cursor-pointer' onClick={ () => setSortOpen( !sortOpen ) }><ArrowDownUp className="w-4 h-4 " />Sort<ChevronDown className="w-4 h-4 opacity-50" /></button>
-                </div>
-
             </div>
 
             {/* the right side of the top bar */}
-            <div className="flex flex-1 gap-4 justify-end">
+            <div className="flex flex-1 gap-4 justify-end sm:flex-none">
                 <button className="flex items-center justify-center gap-4 bg-slate-900 border border-slate-700 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors duration-300 cursor-pointer"><Pencil className="w-4 h-4" /></button>
                 <button className="flex items-center justify-center gap-4 bg-slate-900 border border-slate-700 text-white px-8 py-2 rounded-md hover:bg-blue-700 transition-colors duration-300 cursor-pointer">Create<Plus className="w-4 h-4 mr-2" /></button>
             </div>
