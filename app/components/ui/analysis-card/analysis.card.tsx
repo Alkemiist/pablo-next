76887a9f6b664c67b17118d3c72982b7aa1f13f5
Pablo Villareal <pablovillareal@Mac.attlocal.net>
2025-07-03 17:24:43 -0700

    update on sentiment tab

---
+++ app/components/ui/analysis-card/analysis.card.tsx
@@ -0,0 +1,16 @@
+
+// imports
+
+export function AnalysisCard() {
+
+    // work area
+
+    
+    // return component
+    return (
+        <div className="bg-slate-900 rounded-lg p-4 border border-slate-700 shadow-xl h-[200px] w-[300px]">
+            <h1>Analysis Card</h1>
+        </div>
+    )
+
+}
--- app/profile-analysis/sentiment/page.tsx
+++ app/profile-analysis/sentiment/page.tsx
@@ -1,6 +1,7 @@
 
 // imports
 import { SentimentBar } from "@/app/components/ui/sentiment-bar/sentiment-bar"; 
+import { AnalysisCard } from "@/app/components/ui/analysis-card/analysis.card";
 
 export default function SentimentPage() {
 
@@ -11,6 +12,23 @@ export default function SentimentPage() {
 
         <div className="ml-1 h-[calc(100vh-60px)]">
             <SentimentBar />
+            <div className="flex flex-wrap justify-between gap-6 mt-4 mx-4">
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+                <AnalysisCard />
+            </div>
         </div>
     )
 
